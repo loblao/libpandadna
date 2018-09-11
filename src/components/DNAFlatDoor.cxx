af4da6999ec54a9c584f78895feb5a4cd53bce8d
loblao <12ksit@gmail.com>
2018-09-11 20:22:17 -0300

    Fix visual glitch with DNAWindows and DNAFlatDoor

--- src/components/DNAFlatDoor.cxx
+++ src/components/DNAFlatDoor.cxx
@@ -25,11 +25,12 @@ void DNAFlatDoor::traverse(NodePath& np, DNAStorage* store)
     {
         raise_code_not_found();
     }
-    
+
     NodePath _np = result.copy_to(np);
     _np.set_scale(NodePath(), 1);
     _np.set_x(.5);
     _np.set_hpr(_np, 0);
     _np.set_color(m_color);
     _np.node()->set_effect(DecalEffect::make());
+    _np.set_depth_offset(1);
 }
--- src/components/DNAWindows.cxx
+++ src/components/DNAWindows.cxx
@@ -23,7 +23,7 @@ void DNAWindows::make_from_dgi(DatagramIterator& dgi, DNAStorage* store)
     m_code = dgi.get_string();
     m_color = DGI_EXTRACT_COLOR;
     m_window_count = dgi.get_uint8();
-    m_window_count = m_window_count <= MAX_WINDOWS ? m_window_count : MAX_WINDOWS; 
+    m_window_count = m_window_count <= MAX_WINDOWS ? m_window_count : MAX_WINDOWS;
 }
 
 #define MW(x, y, flip) make_windows(offset + x, offset + y, np, m_code, scale, m_color, store, flip);
@@ -32,41 +32,41 @@ void DNAWindows::traverse(NodePath& np, DNAStorage* store)
 {
     if (!m_window_count)
         return;
-        
+
     float parent_sx = np.get_parent().get_scale().get_x();
     float scale = 0.00375;
 
     if (parent_sx <= 5)
         scale++;
-        
+
     else if (parent_sx <= 10)
         scale += 1.15;
-        
+
     else
         scale += 1.3;
-        
+
     float offset = 0.00375;
-    
+
     switch (m_window_count)
     {
         case 1:
         {
             MW(.5, .5, false)
         }; break;
-        
+
         case 2:
         {
             MW(.33, .5, false)
             MW(.66, .5, true)
         }; break;
-        
+
         case 3:
         {
             MW(.33, .66, false)
             MW(.66, .66, true)
             MW(.5, .33, false)
         }; break;
-        
+
         case 4:
         {
             MW(.33, .25, false)
@@ -74,10 +74,10 @@ void DNAWindows::traverse(NodePath& np, DNAStorage* store)
             MW(.33, .66, false)
             MW(.66, .66, true)
         }; break;
-        
+
         default:
         {
-            std::cerr << "dna(error): m_window_count invalid: " << m_window_count << std::endl;
+            dna_cat.error() << "m_window_count invalid: " << m_window_count << std::endl;
         }; break;
     }
 }
@@ -91,18 +91,17 @@ void DNAWindows::make_windows(float x, float y, NodePath& parent_node,
     ss << code.substr(0, code.size() - 1);
     ss << (flip ? "l" : "r");
     std::string _code = ss.str();
-    
+
     NodePath result = store->find_node(_code);
     if (result.is_empty())
     {
         raise_code_not_found(_code.c_str());
         return;
     }
-    
+
     NodePath window = result.copy_to(parent_node);
     window.set_color(color);
     window.set_scale(NodePath(), scale);
     window.set_pos(x, 0, y);
-    window.set_depth_offset(0);
+    window.set_depth_offset(1);
 }
-
