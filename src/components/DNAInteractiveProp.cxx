93fdfffaaec1abc5f333352cbfa7ddb4cb28af76
John Cote <jwcotejr@gmail.com>
2019-01-22 16:55:46 -0500

    DNAInteractiveProp: Properly set DNACellIndex tag
    Co-Authored-By: loblao <loblao@users.noreply.github.com>

--- src/components/DNAInteractiveProp.cxx
+++ src/components/DNAInteractiveProp.cxx
@@ -22,27 +22,28 @@ void DNAInteractiveProp::make_from_dgi(DatagramIterator& dgi, DNAStorage* store)
 void DNAInteractiveProp::traverse(NodePath& np, DNAStorage* store)
 {
     NodePath _np;
-    
+
     if (m_code == "DCS")
     {
         PT(ModelNode) node = new ModelNode(m_name);
         node->set_preserve_transform(ModelNode::PT_net);
         _np = np.attach_new_node(node);
     }
-    
+
     else
     {
         /// XXX TO DO: Check find_node result
         _np = store->find_node(m_code).copy_to(np);
         _np.set_name(m_name);
     }
-    
+
     _np.set_tag("DNAAnim", m_anim_name);
 
+    const char cell_index = (char) m_cell_id;
     std::stringstream ss;
-    ss << m_cell_id;
+    ss << cell_index;
     _np.set_tag("DNACellIndex", ss.str());
-    
+
     _np.set_pos_hpr_scale(m_pos, m_hpr, m_scale);
     _np.set_color_scale(m_color);
     traverse_children(_np, store);
