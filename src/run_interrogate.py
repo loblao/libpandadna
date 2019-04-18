422fbd856fb136aeaa7e24768c33cad3a79d811b
loblao <12ksit@gmail.com>
2019-04-17 21:52:29 -0300

    CMakeLists: Suppress warnings caused by Eigen

--- src/run_interrogate.py
+++ src/run_interrogate.py
@@ -21,7 +21,7 @@ def interrogate(module):
     cmd += ' -D__inline -DCPPPARSER -DP3_INTERROGATE=1 -D__cplusplus -fnames -string -refcount -assert'
     cmd += ' -S"%(pandadir)s/include/parser-inc" -S"%(pandadir)s/include" -I"%(pandadir)s/include" -I"%(srcdir)s/base" -I"%(srcdir)s/suit" -I"%(srcdir)s/components"'
     cmd += ' -srcdir "%(srcdir)s/%(module)s" -oc "%(srcdir)s/%(module)s_igate.cxx" -od "%(srcdir)s/%(module)s.in" -python-native -DCPPPARSER -D__STDC__=1'
-    cmd += ' -D__cplusplus -D__inline -longlong __int64 -D_X86_ -DWIN32_VC -DWIN32 -module libpandadna -library %(module)s -Dvolatile='
+    cmd += ' -D__cplusplus -D__inline -D_X86_ -DWIN32_VC -DWIN32 -module libpandadna -library %(module)s -Dvolatile='
 
     cmd = cmd % {'pandadir': pandadir, 'module': module, 'srcdir': srcdir}
     files = glob.glob(os.path.join(srcdir, module, '*.h'))
